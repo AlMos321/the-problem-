10b2b2c267490449437fdc494063f2835f582a27
Alex <alexmoss09@gmail.com>
2015-01-05 20:34:40 +0200

    Add contact form

--- src/AlmosBundle/Controller/PageController.php
+++ src/AlmosBundle/Controller/PageController.php
@@ -1,9 +1,14 @@
 <?php
-// src/Blogger/BlogBundle/Controller/PageController.php
+
+
 
 namespace AlmosBundle\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use AlmosBundle\Entity\Enquiry;
+use AlmosBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -15,5 +20,28 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('AlmosBundle:Page:about.html.twig');
+    }
+
+    public function contactAction()
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                // Выполнение некоторого действия, например, отправка письма.
+
+                // Редирект - это важно для предотвращения повторного ввода данных в форму,
+                // если пользователь обновил страницу.
+                return $this->redirect($this->generateUrl('AlmosBundle_contact'));
+            }
+        }
+
+            return $this->render('AlmosBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
