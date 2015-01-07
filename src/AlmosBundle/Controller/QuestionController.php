85cae89dddeb15234291f9578c91b84ddc7c4c41
AlMos321 <alexmoss09@gmail.com>
2015-01-07 15:13:46 +0200

    Add Entity Question. Add DataFixtures Bundle. Create database. Completed database some test date. Add some css style.

---
+++ src/AlmosBundle/Controller/QuestionController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace AlmosBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Question controller.
+ */
+
+class QuestionController extends Controller
+{
+    /**
+     * Show a question entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $quest = $em->getRepository('AlmosBundle:Question')->find($id);
+
+        if (!$quest) {
+            throw $this->createNotFoundException('Unable to fount question.');
+        }
+
+        return $this->render('AlmosBundle:Question:show.html.twig', array(
+            'quest'      => $quest,
+        ));
+    }
+}
